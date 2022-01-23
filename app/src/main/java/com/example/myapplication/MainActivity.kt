b530e169dfc92512ba33fa05198941154e27ae07
lukam <lukamania2003@gmail.com>
2022-01-23 16:01:59 +0400

    Fixed Some Name Bugs

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var chavchavadzebtn_main: ImageButton
+    lateinit var orbeliani_main: ImageButton
+    lateinit var dochanashvili_main:Image
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var chavchavadzebtn_main = findViewById<ImageButton>(R.id.chavchavadzebtn_main)
+        var orbeliani_main = findViewById<ImageButton>(R.id.orbeliani_main)
+        var dochanashvili_main = findViewById<ImageButton>(R.id.dochanashvili_main)
+
+
+        chavchavadzebtn_main.setOnClickListener() {
+            var xintent = Intent(this, MgzavrisWerilebi::class.java)
+            startActivity(xintent)
+
+        }
+
+        orbeliani_main.setOnClickListener() {
+            var dintent = Intent(this, SulkhanSabaPage::class.java)
+            startActivity(dintent)
+        }
+        dochanashvili_main.setOnClickListener() {
+            var dintent = Intent(this, DochanashviliPage::class.java)
+            startActivity(dintent)
+        }
+
     }
 }
--- app/src/main/java/com/example/myapplication/SulkhanSabaPage.kt
+++ app/src/main/java/com/example/myapplication/SulkhanSabaPage.kt
@@ -3,9 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity4 : AppCompatActivity() {
+class SulkhanSabaPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main4)
+        setContentView(R.layout.activity_sulkhansaba_page)
     }
 }
