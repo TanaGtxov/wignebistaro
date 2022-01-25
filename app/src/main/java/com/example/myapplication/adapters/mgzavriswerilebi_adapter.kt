30e1c5ea2d246fbab8a69672d53b4bc1da61b0de
lukam <lukamania2003@gmail.com>
2022-01-25 16:17:35 +0400

    Added Some Pages

--- app/src/main/java/com/example/myapplication/adapters/mgzavriswerilebi_adapter.kt
+++ app/src/main/java/com/example/myapplication/adapters/mgzavriswerilebi_adapter.kt
@@ -3,8 +3,7 @@ package com.example.myapplication.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.myapplication.fragments.firstpage_mgzavriswerilebi
-import com.example.myapplication.fragments.secondpage_mgzavriswerilebi
+import com.example.myapplication.fragments.*
 
 class mgzavriswerilebi_adapter(activity:FragmentActivity) : FragmentStateAdapter(activity) {
 
@@ -16,6 +15,9 @@ class mgzavriswerilebi_adapter(activity:FragmentActivity) : FragmentStateAdapter
         return when(position){
             0 -> firstpage_mgzavriswerilebi()
             1 -> secondpage_mgzavriswerilebi()
+            2 -> thirdpage_mgzavriswerilebi()
+            3 -> fourthpage_mgzavriswerilebi()
+            4 -> fifthpage_mgzavriswerilebi()
             else -> firstpage_mgzavriswerilebi()
         }
     }
---
+++ app/src/main/java/com/example/myapplication/fragments/fourthpage_mgzavriswerilebi.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class fourthpage_mgzavriswerilebi: Fragment(R.layout.fourthpage_mgzavriswerilebi) {
+}
